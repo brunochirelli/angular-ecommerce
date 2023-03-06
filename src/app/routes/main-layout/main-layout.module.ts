932fa84a6a2ebae072dc2944532a89b32f8465df
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-06 21:36:55 +0000

    add page header and footer components

--- src/app/routes/main-layout/main-layout.module.ts
+++ src/app/routes/main-layout/main-layout.module.ts
@@ -2,12 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProductsModule } from 'app/features/products/products.module';
 
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { MainLayoutComponent } from './main-layout.component';
 import { HomeComponent } from '../home/home.component';
 
 @NgModule({
-  declarations: [MainLayoutComponent, HomeComponent],
+  declarations: [
+    MainLayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, MainLayoutRoutingModule, ProductsModule],
 })
 export class MainLayoutModule {}
