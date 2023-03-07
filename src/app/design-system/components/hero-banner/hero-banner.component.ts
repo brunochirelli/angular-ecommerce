99fa32df651c8435f711819a40e6afaa2ed549b6
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-07 22:46:55 +0000

    add hero banner bootstrap

--- src/app/design-system/components/hero-banner/hero-banner.component.ts
+++ src/app/design-system/components/hero-banner/hero-banner.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-banner',
   templateUrl: './hero-banner.component.html',
-  styleUrls: ['./hero-banner.component.css']
+  styleUrls: ['./hero-banner.component.css'],
 })
 export class HeroBannerComponent {
-
+  @Input() title!: string;
+  @Input() subtitle!: string;
 }
