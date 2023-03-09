95a0e420ac7a1848d930917db58aea4659feb475
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-09 15:56:53 +0000

    add 500 error page

---
+++ src/app/routes/page500/page500.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './page500.component.html',
+  styleUrls: ['./page500.component.css']
+})
+export class Page500Component {
+
+}
