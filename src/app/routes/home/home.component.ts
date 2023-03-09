9ed9818a4877ceed8cbdc8112270d2bb548fd4c5
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-09 16:45:20 +0000

    remove unused functions

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -10,7 +10,6 @@ import { map, Observable } from 'rxjs';
 export class HomeComponent implements OnInit {
   productsViewed: Product[] = [];
   count = 0;
-  prod$: Observable<Product> = this.productsService.productTest$;
   prodOld$: Observable<Product> = this.productsService.productOld$;
   products$: Observable<Product[]> = this.productsService.products$.pipe(
     map((products: Product[]) => products.slice(0, 4))
@@ -21,14 +20,6 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.productsService.productOld$.subscribe((product) => {
       this.productsViewed = [...this.productsViewed, product];
-    });
-  }
-
-  getNextProduct() {
-    this.products$.subscribe((products) => {
-      this.productsService.productOld$.next(products[this.count]);
-      this.count++;
-      this.productsService.productTest$.next(products[this.count]);
     });
   }
 }
