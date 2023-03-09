2ef407c18ce65ebf9e34a84f03592e98e5d43629
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-09 16:26:09 +0000

    add cart guard tests deactivate

---
+++ src/app/features/cart/cart.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartGuard implements CanDeactivate<unknown> {
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return window.confirm('Are you sure you want to leave this page?');
+  }
+}
